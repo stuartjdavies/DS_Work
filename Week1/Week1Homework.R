f57e67f3b46d6c806aa0517ed8873953e5c9bab9
stuartjdavies <stuart_j_davies@hotmail.com>
2015-08-10 11:48:59 +1000

    Added R markdown example

--- Week1/Week1Homework.R
+++ Week1/Week1Homework.R
@@ -1,8 +1,18 @@
+---
+title: "Homework week 2 - Solution"
+author: "Stuart Davies"
+date: "10 August 2015"
+output: html_document
+---
+  
 install.packages("dplyr")
 install.packages("Hmisc")
 install.packages("stingr")
 install.packages("reshape")
 install.packages("zoo")
+install.packages("knitr")
+install.packages("xtable")
+
 library(dplyr)
 library(stringr)
 library(reshape2)
@@ -16,8 +26,9 @@ load("data/complete_data.RData")
 
 avgReturns <- melt(SA2,id=c("Name", "Global Broad Category Group","Global Category","Morningstar Category", "MPT Benchmark"))  %>% 
                         mutate(Date = paste0(str_extract(variable, "[0-9]{4}-[0-9]{2}"), "-01") %>% as.Date) %>% 
-                        select(Date, Name, value) %>% filter(!is.na(value)) %>%
-                        group_by(Name) %>% summarise(`Average returns` = mean(value)) %>% collect %>% 
+                        filter(!is.na(value)) %>%
+                        group_by(Name) %>% 
+                        summarise(`Average returns` = mean(value)) %>% collect %>% 
                         arrange(desc(`Average returns`))
 
 #
@@ -27,16 +38,12 @@ invoices <- read.csv("data/invoices.csv")
 
 monthlySales <- invoices %>%
                   mutate(`Year and month`=as.yearmon(as.POSIXlt(strptime(gsub("[A-Z]{4}", "", date), "%a %b %d %H:%M:%S %Y")), "%Y %m")) %>%
-                  select(`Year and month`,total) %>%
                   group_by(`Year and month`) %>% 
                   summarise(`Total`=sum(total)) %>% 
-                  collect %>% 
                   arrange(desc(`Year and month`))
 
 dailySales <- invoices %>%
                 mutate(`Year, month and day`=as.Date(as.POSIXlt(strptime(gsub("[A-Z]{4}", "", date), "%a %b %d %H:%M:%S %Y")))) %>%
-                select(`Year, month and day`,total) %>%
                 group_by(`Year, month and day`) %>% 
                 summarise(`Total`=sum(total)) %>% 
-                collect %>% 
                 arrange(desc(`Year, month and day`))
